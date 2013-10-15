e75c8f8ba17a1abf169079a12c186017bebc3acf
unknown <olamiraux@PRESTA-DEV1.prosys.priv>
2013-10-15 13:13:54 +0200

    Remove old files

--- spec/MapSpec-old.js
+++
@@ -1,8 +0,0 @@
-describe("Map", function() {
-  it("should be create with width and height", function() {
-    var map = new AFRO.Map(10, 10);
-    
-    expect(map.height).toEqual(10);
-    expect(map.width).toEqual(10);
-  });
-});
